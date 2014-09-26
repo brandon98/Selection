8b1b80c7634f91333a9e0ab8dc6fefaa0f7817e3
Brandon Dickson <40203@longroad.ac.uk>
2014-09-26 16:01:01 +0100

    Selection exercises

---
+++ Selection revision exercise 2.py
@@ -0,0 +1,10 @@
+#Brandon Dickson
+#Selection revision exercise 2
+#26-09-2104
+
+age=int(input("Please enter your age:"))
+
+if age >= 17:
+    print("You are old enough to drive.")
+else:
+    print("You're not old enough to drive.")
---
+++ Selection revision exercise 3.py
@@ -0,0 +1,9 @@
+#Brandon Dickson
+#Selection revision exercise 3
+#26-09-2104
+
+number=int(input("Please enter a number:"))
+if number >= 21 and number <= 29:
+    print("Number in range.")
+else:
+    print("Number not in range.")
