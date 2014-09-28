cb23f23bf3f45b4aa3b38a7cccb082e922796676
brandon98 <40203@longroad.ac.uk>
2014-09-28 20:52:29 +0100

    selection revision exercise 4

---
+++ selection revision exercise 4.py
@@ -0,0 +1,16 @@
+#Brandon Dickson
+#Selection revision exercise 4
+#28-09-14
+
+number_1=int(input("Please enter a number:"))
+number_2=int(input("Please enter a number:"))
+
+if number_1 > number_2:
+               print("Highest Number:", number_1)
+
+else:
+               print("Highest Number:", number_2)
+
+
+
+               
