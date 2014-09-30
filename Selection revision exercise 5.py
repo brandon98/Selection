b127daf3e889d0cafa6837d28cae8a2ff2eda1b1
Brandon Dickson <40203@longroad.ac.uk>
2014-09-30 10:28:04 +0100

    selection revision exercise 5

---
+++ Selection revision exercise 5.py
@@ -0,0 +1,17 @@
+#Brandon Dickson
+#Selection revision exercise 5
+#26-09-2104
+
+number=int(input("Please enter a number:"))
+
+if number >= 21 and number <= 29:
+    print("Number in range.")
+
+elif number < 21:
+    print("Number below range.")
+
+elif number > 29:
+    print("Number above range.")
+
+else:
+    print("Number not in range.")
