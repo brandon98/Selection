54d8a8020091bd79120354b06da81a873572c796
Brandon Dickson <40203@longroad.ac.uk>
2014-09-30 10:33:36 +0100

    selection revision exercise 6

---
+++ selection revision exercise 6.py
@@ -0,0 +1,20 @@
+#Brandon Dickson
+#Selection revision exercise 6
+#28-09-14
+
+number_1=int(input("Please enter a number:"))
+number_2=int(input("Please enter a number:"))
+number_3=int(input("Please enter a number:"))
+
+if number_1 > number_2 and number_1 > number_3:
+               print("Highest Number:", number_1)
+
+elif number_2 > number_1 and number_2 > number_3:
+    print("Highest Number:", number_2)
+
+else:
+    print("Highest Number:", number_3)
+
+
+
+               
