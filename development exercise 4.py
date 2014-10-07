6053091771c20ea0ac2be4bd22706909dc31355a
Brandon Dickson <40203@longroad.ac.uk>
2014-10-07 10:38:42 +0100

    development exercise 4

---
+++ development exercise 4.py
@@ -0,0 +1,26 @@
+#Brandon Dickson
+#Develpment exercise 4
+#07-10-2014
+
+mark= int(input("Please enter your mark for the exam:"))
+
+if mark >= 81 and mark <= 100:
+    print("Grade A.")
+
+elif mark >= 71 and mark <= 80:
+    print("Grade B.")
+
+elif mark >= 61 and mark <= 70:
+    print("Grade C.")
+
+elif mark >= 51 and mark <= 60:
+    print("Grade D.")
+
+elif mark >= 41 and mark <= 50:
+    print("Grade E.")
+
+elif mark >= 31 and mark <= 40:
+    print ("Grade U.")
+
+else:
+    print("No grade.")
